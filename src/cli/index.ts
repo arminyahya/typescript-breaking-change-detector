683c0e7892a939009632e02b94883e3d2ae7fc9a
armin <arminyahyaa@gmail.com>
2022-03-27 22:38:17 +0430

    REFACTOR: Add commander to cli

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,7 +1,13 @@
-import api from "./api"
+import api from "./api";
+import { Command } from "commander";
+
+const program = new Command();
+program.option("-project, --project <char>");
+program.parse();
 
 export default function cli() {
-	api({projectRoot: '../..'});
+  const projectRoot = program.getOptionValue("project");
+  api({ projectRoot: projectRoot });
 }
 
-cli()
+cli();
