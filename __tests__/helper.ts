e7acb008e40c9df7bbd685d505ba82929ced6238
arminyahya <arminyahyaa@gmail.com>
2024-11-27 18:51:00 +0330

    chore: change some variable names in __tests__ folder to avoid confusion

--- __tests__/helper.ts
+++ __tests__/helper.ts
@@ -3,19 +3,19 @@ import isNewDeclarationValid from "../src";
 import { generateContext, getErrorInfo, inValidDeclareErrorForTest, pareCode } from "../src/helper";
 
 function getTwoParsedCodeAndContext(code1: string, code2: string) {
-  const parsedCode1 = pareCode(code1);
-  const parsedCode2 = pareCode(code2);
+  const parsedPrevCode = pareCode(code1);
+  const parsedCurrentCode = pareCode(code2);
   const context = generateContext(code1, code2);
-  return { context, parsedCode1, parsedCode2 };
+  return { context, parsedPrevCode, parsedCurrentCode };
 }
 
 export default function testRunner(name, prevCode, currentCode, errorType, errorMessage) {
 	test(name, () => {
-    const { context, parsedCode1, parsedCode2 } = getTwoParsedCodeAndContext(
+    const { context, parsedPrevCode, parsedCurrentCode } = getTwoParsedCodeAndContext(
       prevCode,
       currentCode
     );
-    const {isValid, info} = isNewDeclarationValid(context, parsedCode1, parsedCode2);
+    const {isValid, info} = isNewDeclarationValid(context, parsedPrevCode, parsedCurrentCode);
 		expect(isValid).toBe(false);
     expect(info.replace(/\s/g, "")).toContain(
 			chalk.red("Error: " + getErrorInfo(errorType, errorMessage)).replace(/\s/g, "")
