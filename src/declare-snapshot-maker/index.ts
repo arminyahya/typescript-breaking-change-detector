e2016563ae7da996359969f66146cd4bd559a420
arminyahya <arminyahyaa@gmail.com>
2025-03-13 15:59:23 +0330

    refactor: replace copyFileSync with copySync and improve code formatting

--- src/declare-snapshot-maker/index.ts
+++ src/declare-snapshot-maker/index.ts
@@ -1,27 +1,29 @@
-import { copyFileSync, existsSync, mkdirSync } from "fs-extra";
+import { copySync, existsSync, mkdirSync } from "fs-extra";
 import path from "path";
 import { OPERATION_PATH, PREV_DECLARATION_PATH } from "../constants/filenames";
 import { getFullFileName } from "./helper";
 const requiredFolders = [OPERATION_PATH, PREV_DECLARATION_PATH];
 
 interface Config {
-	projectRoot: string;
+  projectRoot: string;
   declarationFiles: string[];
 }
 
 function makeFolderIfNotExist(folder: string, projectRoot: string) {
-	if (!existsSync(path.join(projectRoot,  folder))) {
-    mkdirSync(path.join(projectRoot,  folder));
+  if (!existsSync(path.join(projectRoot, folder))) {
+    mkdirSync(path.join(projectRoot, folder));
   }
 }
 
 function createOperationFolders(projectRoot: string) {
-	requiredFolders.forEach((folder) => makeFolderIfNotExist(folder, projectRoot));
+  requiredFolders.forEach((folder) => makeFolderIfNotExist(folder, projectRoot));
 }
 
-export default function declarationSnapShotMaker({ projectRoot , declarationFiles }: Config) {
+export default function declarationSnapShotMaker({ projectRoot, declarationFiles }: Config) {
   createOperationFolders(projectRoot);
   declarationFiles.forEach((file) => {
-    copyFileSync(path.join(projectRoot, file), path.join(projectRoot, PREV_DECLARATION_PATH + `/${getFullFileName(file)}`));
+    const srcPath = path.join(projectRoot, file);
+    const destPath = path.join(projectRoot, PREV_DECLARATION_PATH, file);
+    copySync(srcPath, destPath);
   });
 }
