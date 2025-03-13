037f909485cd50567f871ba52033fd77d777494b
arminyahya <arminyahyaa@gmail.com>
2025-03-13 14:12:58 +0330

    fix: improve error handling in Typescript detector

--- src/cli/api.ts
+++ src/cli/api.ts
@@ -5,11 +5,11 @@ import { CONFIG_FILENAME, PREV_DECLARATION_PATH } from "../constants/filenames";
 import declarationSnapShotMaker from "../declare-snapshot-maker";
 import { readFileSync } from "fs";
 import chalk from "chalk";
-import { error } from "console";
 
 interface ApiConfig {
   projectRoot: string;
 }
+
 export default function api(config: ApiConfig) {
   const { projectRoot } = config;
   const configFile = readFileSync(path.join(
@@ -17,17 +17,17 @@ export default function api(config: ApiConfig) {
     CONFIG_FILENAME
   ), 'utf-8');
 
-  const {declarationFiles} = JSON.parse(configFile);
-	// its first time running detector
-	if (!existsSync(path.join(projectRoot,  PREV_DECLARATION_PATH))) {
-		declarationSnapShotMaker({ projectRoot, declarationFiles });
-	} else {
-		const validationResult = areDeclareFilesValid({ projectRoot, declarationFiles });
-		if(validationResult.isValid) {
-			declarationSnapShotMaker({ projectRoot, declarationFiles });
-		} else {
-			console.error(validationResult.info)
-			throw new error('Error detected during Typescript breaking change detector code analyzing');
-		}
-	}
+  const { declarationFiles } = JSON.parse(configFile);
+  // its first time running detector
+  if (!existsSync(path.join(projectRoot, PREV_DECLARATION_PATH))) {
+    declarationSnapShotMaker({ projectRoot, declarationFiles });
+  } else {
+    const validationResult = areDeclareFilesValid({ projectRoot, declarationFiles });
+    if (validationResult.isValid) {
+      declarationSnapShotMaker({ projectRoot, declarationFiles });
+    } else {
+      const errorMessage = `Error detected during Typescript breaking change detector code analyzing\n${validationResult.info}`;
+      throw new Error(errorMessage);
+    }
+  }
 }
